558c26fd3e77bed5aa18ea954cb8c66331f6c63d
Vlad <t.vladislav@bow.group>
2022-04-04 12:17:39 +0500

    ✨ linux driver

---
+++ app/drivers/linux_driver.py
@@ -0,0 +1,28 @@
+from enum import Enum
+import subprocess
+
+
+class CommandOption(Enum):
+    START = "start"
+    STOP = "stop"
+    RESTART = "restart"
+
+
+class LinuxDriver:
+    SERVICE_NAME = "bluetooth"
+
+    @classmethod
+    def _prepare_command(cls, command: CommandOption):
+        return ["systemctl", command.value, cls.SERVICE_NAME]
+
+    @classmethod
+    def start(cls):
+        subprocess.run(cls._prepare_command(CommandOption.START))
+
+    @classmethod
+    def stop(cls):
+        subprocess.run(cls._prepare_command(CommandOption.STOP))
+
+    @classmethod
+    def restart(cls):
+        subprocess.run(cls._prepare_command(CommandOption.RESTART))
--- app/routers/daemon.py
+++ app/routers/daemon.py
@@ -1,5 +1,7 @@
 from aiohttp import web
 from app.models import StateResponseModel
+from app.drivers.linux_driver import LinuxDriver
+
 
 prefix = "/daemon"
 daemon_router = web.RouteTableDef()
@@ -12,14 +14,17 @@ async def state(request):
 
 @daemon_router.post(prefix + "/enable")
 async def enable(request):
+    LinuxDriver.start()
     return web.Response(body=StateResponseModel(state=True).json())
 
 
 @daemon_router.post(prefix + "/disable")
 async def enable(request):
+    LinuxDriver.stop()
     return web.Response(body=StateResponseModel(state=True).json())
 
 
 @daemon_router.post(prefix + "/restart")
 async def enable(request):
+    LinuxDriver.restart()
     return web.Response(body=StateResponseModel(state=True).json())
